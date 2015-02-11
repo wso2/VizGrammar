795739e90bb29a7ab5c92b02c0f25e2f17fe236d
Dunith Dhanushka <dunithd@gmail.com>
2015-02-11 14:49:05 +0530

    Adding table sample

--- igviz.js
+++ igviz.js
@@ -35,6 +35,7 @@
 	};
 
 
+	//Plots a chart in a given div specified by canvas
 	igviz.plot = function(canvas, config, dataTable) {
 		if (config.chartType == "bar") {
 			this.drawBarChart(canvas, config, dataTable);
@@ -1025,6 +1026,139 @@
 				}
 			});
 	}
+
+
+	/////////////////////////////////////////// TODO: Fawsan's util stuff. Better refactor these later ////////////////////
+
+	/**
+	 * Get teh maximum of a numaric array
+	 * @param data
+	 * @returns {*}
+	 */
+	function getMax(data) {
+
+	    var max = data[0];
+
+	    for (var i = 0; i < data.length; i++) {
+	        if (max < data[i]) {
+	            max = data[i];
+	        }
+	    }
+	    return max;
+	}
+
+	/**
+	 * Get the minimum value of a numeric array
+	 * @param data
+	 * @returns {*}
+	 */
+	function getMin(data) {
+
+	    var min = data[0];
+
+	    for (var i = 0; i < data.length; i++) {
+	        if (min > data[i]) {
+	            min = data[i];
+	        }
+	    }
+	    return min;
+	}
+
+	/**
+	 * Get the average of a numeric array
+	 * @param data
+	 * @returns average
+	 */
+	function getAvg(data) {
+
+	    var sum = 0;
+
+	    for (var i = 0; i < data.length; i++) {
+	        sum = sum + data[i];
+	    }
+
+	    var average = (sum / data.length).toFixed(4);
+	    return average;
+	}
+
+	/**
+	 * Function to calculate the standard deviation
+	 * @param values
+	 * @returns sigma(standard deviation)
+	 */
+	function standardDeviation(values) {
+	    var avg = getAvg(values);
+
+	    var squareDiffs = values.map(function (value) {
+	        var diff = value - avg;
+	        var sqrDiff = diff * diff;
+	        return sqrDiff;
+	    });
+
+	    var avgSquareDiff = getAvg(squareDiffs);
+
+	    var stdDev = Math.sqrt(avgSquareDiff);
+	    return stdDev;
+	}
+
+	/**
+	 * Get the p(x) : Helper function for the standard deviation
+	 * @param x
+	 * @param sigma
+	 * @param u
+	 * @returns {number|*}
+	 */
+	function pX(x, sigma, u) {
+
+	    p = (1 / Math.sqrt(2 * Math.PI * sigma * sigma)) * Math.exp((-(x - u) * (x - u)) / (2 * sigma * sigma));
+
+	    return p;
+	}
+
+
+	/**
+	 * Get the normalized values for a list of elements
+	 * @param xVals
+	 * @returns {Array} of normalized values
+	 *
+	 */
+	function NormalizationCoordinates(xVals) {
+
+	    var coordinates = [];
+
+	    var u = getAvg(xVals);
+	    var sigma = standardDeviation(xVals);
+
+	    for (var i = 0; i < xVals.length; i++) {
+
+	        coordinates[i] = {x: xVals[i], y: pX(xVals[i], sigma, u)};
+	    }
+
+	    return coordinates;
+	}
+
+	/**
+	 * This function will extract a column from a multi dimensional array
+	 * @param 2D array
+	 * @param index of column to be extracted
+	 * @return array of values
+	 */
+
+	function parseColumnFrom2DArray(dataset, index) {
+
+	    var array = [];
+
+	    //console.log(dataset.length);
+	    //console.log(dataset[0].data);
+	    //console.log(dataset[1].data);
+
+	    for (var i = 0; i < dataset.length; i++) {
+	        array.push(dataset[i][index])
+	    }
+
+	    return array;
+	}
+
 
 
 
