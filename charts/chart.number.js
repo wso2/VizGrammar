ad0ab4034d4e68cce3223763d36e2bf7ecaf3030
Tharik Kanaka <onestepfromhell@hotmail.com>
2015-12-16 18:00:24 +0530

    Number chart improved

--- charts/chart.number.js
+++ charts/chart.number.js
@@ -14,9 +14,15 @@ var number = function(dataTable, config) {
 number.prototype.draw = function(div) {
   div = div.replace("#","");
   var contentId = div+"Content";
-  var divContent = "<p style='padding: 0px 0px 0px 20px;'>"+config.title+"</p>"
+  var textContent = "";
+
+  if (this.data != null && this.data.length != 0) {
+      textContent = this.data[this.data.length-1][this.metadata.names[this.config.x]];    
+  }
+
+  var divContent = "<p style='padding: 0px 0px 0px 20px;'>"+this.config.title+"</p><br/>"
                   +"<p style='font-size:60;padding: 0px 0px 0px 20px;' id='"+contentId+"'>"
-                  +this.data[data.length-1][this.metadata.names[this.config.x]]+"</p>";
+                  +textContent+"</p>";
 
    document.getElementById(div).innerHTML = divContent;
    this.view = contentId;
