97f00ad42ad43c10eeffb139f928f1ec8c5b0b20
Damith <damith@damith-Latitude-E6540>
2016-01-26 11:58:35 +0530

    Added map chart

--- charts/chart.util.js
+++ charts/chart.util.js
@@ -30,6 +30,18 @@ function checkConfig(config, metadata){
 		config.color = metadata.names.indexOf(config.color);
 	}
 
+    if (config.mapType == null) {
+        config.mapType = -1;
+    }
+
+    if (config.minColor == null) {
+        config.minColor = -1;
+    }
+
+    if (config.maxColor == null) {
+        config.maxColor = -1;
+    }
+
     if (config.size == null) {
         config.size = -1;
     } else {
@@ -53,7 +65,7 @@ function checkConfig(config, metadata){
 	}
 
 	if (config.padding == null) {
-		config.padding = {"top": 30, "left": 50, "bottom": 100, "right": 100};
+        config.padding = {"top": 20, "left": 60, "bottom": 40, "right": 50};
 	}
 
 	config.x = metadata.names.indexOf(config.x);
