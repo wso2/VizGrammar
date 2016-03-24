b990d62af04c5a8e2f690de6f70ea394c1490dd0
Tharik Kanaka <tharik.kanaka@gmail.com>
2016-03-24 17:00:18 +0530

    offset space between bar charts increased

--- charts/chart.bar.js
+++ charts/chart.bar.js
@@ -247,7 +247,7 @@ function getBarMark(config, metadata){
   if (config.orientation == "left") {
     markContent = {
                     "y": {"scale": "x", "field": metadata.names[config.x]},
-                    "height": {"scale": "x", "band": true, "offset": -1},
+                    "height": {"scale": "x", "band": true, "offset": -10},
                     "x": {"scale": "y", "field": metadata.names[config.y]},
                     "x2": {"scale": "y", "value": 0},
                     "fill": {"value": config.markColor},
@@ -256,7 +256,7 @@ function getBarMark(config, metadata){
   } else {
     markContent = {
                     "x": {"scale": "x", "field": metadata.names[config.x]},
-                    "width": {"scale": "x", "band": true, "offset": -1},
+                    "width": {"scale": "x", "band": true, "offset": -10},
                     "y": {"scale": "y", "field": metadata.names[config.y]},
                     "y2": {"scale": "y", "value": 0},
                     "fill": {"value": config.markColor},
@@ -297,7 +297,7 @@ function getStackBarMark(config, metadata){
         "properties": {
           "update": {
             "y": {"scale": "x", "field": metadata.names[config.x]},
-            "height": {"scale": "x", "band": true, "offset": -1},
+            "height": {"scale": "x", "band": true, "offset": -10},
             "x": {"scale": "y", "field": "layout_start"},
             "x2": {"scale": "y", "field": "layout_end"},
             "fill": {"scale": "color", "field": metadata.names[config.color]},
@@ -323,7 +323,7 @@ function getStackBarMark(config, metadata){
         "properties": {
           "update": {
             "x": {"scale": "x", "field": metadata.names[config.x]},
-            "width": {"scale": "x", "band": true, "offset": -1},
+            "width": {"scale": "x", "band": true, "offset": -10},
             "y": {"scale": "y", "field": "layout_start"},
             "y2": {"scale": "y", "field": "layout_end"},
             "fill": {"scale": "color", "field": metadata.names[config.color]},
